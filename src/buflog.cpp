7fcb9fb4b65e38d6661cc469fa433f155d15c9ea
41nya <sny.cho@gmail.com>
2021-02-20 19:57:29 +0900

    Initial commit

---
+++ src/buflog.cpp
@@ -0,0 +1,22 @@
+#include "buflog.h"
+
+#include "sharedbuffer.h"
+
+namespace buflog {
+
+Logger::Logger(const std::string filename, const int size)
+    : bufptr(NULL) {
+    bufptr = new SharedBuffer(filename, size);
+}
+Logger::~Logger() {
+    delete bufptr;
+}
+
+void Logger::Log(std::string msg) {
+
+}
+
+int Logger::Flush() {
+    return 0;
+}
+}   /* buflog */
---
+++ src/errorutil.h
@@ -0,0 +1,21 @@
+#ifndef BUFLOG_INTERNAL_ERRORUTIL_H
+#define BUFLOG_INTERNAL_ERRORUTIL_H
+
+#include <string>
+
+namespace buflog {
+    
+inline void ToErrStr(std::string& errstr, const char* func, const int errnum) {
+    char strerrno[128]{};
+
+    if (-1 == strerror_r(errnum, strerrno, sizeof(strerrno))) {
+        errstr = std::string(func) + std::string(" ") + std::to_string(errnum);
+    }
+    else {
+        errstr = std::string(func) + std::string(" ") + std::string(strerrno);
+    }
+};
+
+} //buflog
+#endif /* BUFLOG_INTERNAL_ERRORUTIL_H */
+
