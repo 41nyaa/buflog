43f627c80415a5167931d5230687a05cb3e92d1e
41nya <sny.cho@gmail.com>
2021-02-27 15:42:43 +0900

    Update setup googletest

--- tests/setup_gtest.sh
+++ tests/setup_gtest.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
 rm -rf googletest
-git clone https://github.com/google/googletest.git
-cd googletest
-cmake .
-make
+git clone https://github.com/google/googletest.git googletest
+cmake -S ./googletest -B ./googletest
+make -C googletest
